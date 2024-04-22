9c5b3e94a8ba5e9be575b2a6d9f0f37e34b28114
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-04-22 15:23:00 +0200

    Updated JBRApiTest to test all services via reflection

--- tests/JBRApiTest.java
+++ tests/JBRApiTest.java
@@ -26,25 +26,36 @@
  * @run main JBRApiTest
  */
 
+import com.jetbrains.Extensions;
 import com.jetbrains.JBR;
-import com.jetbrains.JBRFileDialog;
 
-import java.awt.*;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 
 public class JBRApiTest {
+
+    // These services may not always be supported and have their own dedicated tests.
+    private static final Set<String> IGNORED_SERVICES = Set.of(
+            "com.jetbrains.WindowMove",
+            "com.jetbrains.RoundedCornersManager"
+    );
 
     public static void main(String[] args) throws Exception {
         if (!JBR.getApiVersion().equals("SNAPSHOT") &&
             !JBR.getApiVersion().matches("\\d+\\.\\d+\\.\\d+")) throw new Error("Invalid API version: " + JBR.getApiVersion());
         if (!JBR.isAvailable()) throw new Error("JBR API is not available");
-        checkMetadata();
-        testServices();
+        List<String> knownServices = checkMetadata();
+        testAllKnownServices(knownServices);
+        testPublicServices();
+        testExtensions();
     }
 
-    private static void checkMetadata() throws Exception {
+    private static List<String> checkMetadata() throws Exception {
         Class<?> metadata = Class.forName(JBR.class.getName() + "$Metadata");
         Field field = metadata.getDeclaredField("KNOWN_SERVICES");
         field.setAccessible(true);
@@ -52,14 +63,44 @@ public class JBRApiTest {
         if (!knownServices.contains("com.jetbrains.JBR$ServiceApi")) {
             throw new Error("com.jetbrains.JBR$ServiceApi was not found in known services of com.jetbrains.JBR$Metadata");
         }
+        return knownServices;
     }
 
-    private static void testServices() {
-        Objects.requireNonNull(JBR.getFontExtensions().getSubpixelResolution());
-        Objects.requireNonNull(JBRFileDialog.get(new FileDialog((Frame) null)));
-        Objects.requireNonNull(JBR.getAccessibleAnnouncer());
-        if (!System.getProperty("os.name").toLowerCase().contains("linux")) {
-            Objects.requireNonNull(JBR.getRoundedCornersManager());
+    private static void testAllKnownServices(List<String> knownServices) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        ClassLoader classLoader = JBR.class.getClassLoader();
+        Field serviceApiField = JBR.class.getDeclaredField("api");
+        serviceApiField.setAccessible(true);
+        Object serviceApi = serviceApiField.get(null);
+        Method getServiceMethod = serviceApi.getClass().getDeclaredMethod("getService", Class.class);
+        getServiceMethod.setAccessible(true);
+        for (String serviceName : knownServices) {
+            if (IGNORED_SERVICES.contains(serviceName)) continue;
+            Class<?> serviceClass = Class.forName(serviceName, true, classLoader);
+            Object service = getServiceMethod.invoke(serviceApi, serviceClass);
+            if (service == null) throw new Error("Service " + serviceName + " is not supported");
+        }
+    }
+
+    private static void testPublicServices() {
+        Arrays.stream(JBR.class.getDeclaredMethods())
+                .filter(m -> Modifier.isPublic(m.getModifiers()) && m.getParameterCount() == 0 && m.getName().startsWith("get"))
+                .forEach(m -> {
+                    if (IGNORED_SERVICES.contains(m.getReturnType().getName())) return;
+                    try {
+                        Object service = m.invoke(null);
+                        if (service == null) throw new Error(m.getName() + " returned null");
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    private static void testExtensions() {
+        if (System.getProperty("jetbrains.runtime.api.extensions.enabled", "true").equalsIgnoreCase("false")) return;
+        for (Extensions ext : Extensions.values()) {
+            if (!JBR.isExtensionSupported(ext)) {
+                throw new Error("Extension " + ext.name() + " is not supported");
+            }
         }
     }
 }
