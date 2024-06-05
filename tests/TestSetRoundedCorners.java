e346e5d9dd764db4c1fe7fe6c04d5384bce6a4c9
Nikita Gubarkov <nikita.gubarkov@jetbrains.com>
2024-06-05 16:44:23 +0200

    Apache 2.0

--- tests/TestSetRoundedCorners.java
+++ tests/TestSetRoundedCorners.java
@@ -1,24 +1,26 @@
 /*
- * Copyright 2023 JetBrains s.r.o.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Copyright 2000-2024 JetBrains s.r.o.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * @test
+ * @summary JBR-4563 Rounded corners for native Window on Mac OS
+ * @summary JBR-4787 Rounded corners for native Window on Windows 11
+ * @author Alexander Lobas
+ * @requires (os.family == "mac" | os.family == "windows")
+ * @run main/manual TestSetRoundedCorners
  */
 
 import com.jetbrains.JBR;
@@ -28,14 +30,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.lang.reflect.InvocationTargetException;
 
-/**
- * @test
- * @summary JBR-4563 Rounded corners for native Window on Mac OS
- * @summary JBR-4787 Rounded corners for native Window on Windows 11
- * @author Alexander Lobas
- * @requires (os.family == "mac" | os.family == "windows")
- * @run main/manual TestSetRoundedCorners
- */
 public class TestSetRoundedCorners {
     private static final int TD_RED = 50;
     private static final int TD = 10;
